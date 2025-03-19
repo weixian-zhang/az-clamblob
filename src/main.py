73f2a03e1664bb8c16b647fcbe30bd01d834bf63
wxz <weixianzhang@outlook.com>
2025-03-19 11:51:31 +0800

    fix(iac): updated container app yaml

--- src/main.py
+++ src/main.py
@@ -11,13 +11,17 @@ def scan():
 
     cvm = ClamAVManager(config)
 
-    result1 = cvm.scan_file("/mnt/eicar_com.zip")
+    result1 = cvm.scan_file("/azfile/eicar_com.zip")
 
-    result2 = cvm.scan_file("/mnt/testfile.org-5GB.dat")
+    result2 = cvm.scan_file("/azfile/testfile.org-5GB.dat")
+
+    result3 = cvm.scan_file("/azfile/10GB.bin")
 
     Log.info(f'{result1.file_path} - {result1.status} - {result1.message}')
 
     Log.info(f'{result2.file_path} - {result2.status} - {result2.message}')
+
+    Log.info(f'{result3.file_path} - {result3.status} - {result3.message}')
 
 
 try:
