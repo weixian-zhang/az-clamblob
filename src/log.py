86a20694170619c33b9bc63eaabbaed9bbad85ce
wxz <weixianzhang@outlook.com>
2025-03-26 12:33:05 +0800

    feat: able to scan blob and move to quarantine container

--- src/log.py
+++ src/log.py
@@ -3,6 +3,7 @@ from opencensus.ext.azure.log_exporter import AzureLogHandler
 from config import Config
 from datetime import datetime
 import pytz
+
 config = Config()
 
 logger = logging.getLogger(__name__)
@@ -16,12 +17,11 @@ logger.addHandler(AzureLogHandler(
 def friendly_datetime(time_zone='Asia/Singapore'):
         return datetime.now().astimezone(pytz.timezone(time_zone)).strftime("%a %d %b %Y %H:%M:%S")
 
-def info(message: str):
-    logger.debug(f'{friendly_datetime()}: {message}')
+def info(message: str, module=''):
+    msg = f'{friendly_datetime()} | {module} - {message}' if module else f'{friendly_datetime()} - {message}'
+    logger.debug(msg)
 
-def error(e: Exception):
-    if e is Exception:
-        logger.error(f'{friendly_datetime()}: {str(e)}')
-    else:
-        logger.error(f'{friendly_datetime()}: {e}')
+def error(e: Exception, module=''):
+    msg = f'{friendly_datetime()} | {module} - {str(e)}' if module else f'{friendly_datetime()} - {str(e)}'
+    logger.error(msg)
 
