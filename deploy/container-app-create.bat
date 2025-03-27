2dbf6770f828ec6c19e1fbb6f0ae8ff1b8eb6759
wxz <weixianzhang@outlook.com>
2025-03-27 23:18:40 +0800

    devops: added python scrit to create all container apps

--- deploy/container-app-create.bat
+++
@@ -1,5 +0,0 @@
-az containerapp create -n clamblob-clamav -g rg-clamblob --yaml "clamav_container_app.yaml"
-az containerapp update -n clamblob-scanner -g rg-clamblob --set-env-vars "MAX_FILE_SIZE=40000M" "MAX_SCAN_SIZE=40000M" "MAX_FILES=50000"
-
-az containerapp create -n clamblob-scanner -g rg-clamblob --yaml "clamblob_scanner_container_app.yaml"
-az containerapp update -n clamblob-scanner -g rg-clamblob --set-env-vars "MOUNT_PATH=/mnt" "APP_INSIGHTS_INSTRUMENTATION_KEY=5bb85efc-b516-4116-aebb-65ca9dcd876f" "CLAMAV_HOST=clamblob-clamav" "CLAMAV_PORT=3310" "QUARANTINE_CONTAINER_NAME=quarantine"
