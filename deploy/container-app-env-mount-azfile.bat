ab32ac55ff229f24a94c73eadaffde834bd214a2
wxz <weixianzhang@outlook.com>
2025-03-28 12:19:32 +0800

    devops: updated azure cli cmds to deploy container apps

--- deploy/container-app-env-mount-azfile.bat
+++
@@ -1,9 +0,0 @@
-az containerapp env storage set \
-  --access-mode ReadWrite \
-  --azure-file-account-name $STORAGE_ACCOUNT_NAME \
-  --azure-file-account-key $STORAGE_ACCOUNT_KEY \
-  --azure-file-share-name $STORAGE_SHARE_NAME \
-  --storage-name $STORAGE_MOUNT_NAME \
-  --name $ENVIRONMENT_NAME \
-  --resource-group $RESOURCE_GROUP \
-  --output table
---
+++ deploy/create-containerapp-env-apps.cmd
@@ -0,0 +1,28 @@
+REM create container app environment
+az containerapp env create --assign-identity --enable-workload-profiles --resource-group "rg-clamblob" --name "cae-common" --location "southeastasia" --infrastructure-subnet-resource-id /subscriptions/c9061bc7-fa28-41d9-a783-2600b29c6e2f/resourceGroups/rg-clamblob/providers/Microsoft.Network/virtualNetworks/vnet-clamblob/subnets/CAEnvironment --logs-workspace-id c596239e-beda-4999-a79f-58e34d9881e4
+az containerapp env workload-profile set --resource-group rg-clamblob --name clamblob-workload-profile --workload-profile-type D4 --workload-profile-name dedicated_prof --min-nodes 1 --max-nodes 1
+
+REM mount azure file share to container app environment
+az containerapp env storage set \
+  --access-mode ReadWrite \
+  --azure-file-account-name {$STORAGE_ACCOUNT_NAME} \
+  --azure-file-account-key {$STORAGE_ACCOUNT_KEY} \
+  --azure-file-share-name clamblob-scan \
+  --storage-name clamblob-scan \
+  --name cae-common \
+  --resource-group rg-clamblob \
+  --output table
+
+
+  REM create clamav container app
+  az containerapp create -n clamblob-clamav -g rg-clamblob --yaml "clamav_container_app.yaml"
+
+  REM update clamav container app env var
+  az containerapp update -n clamblob-clamav -g rg-clamblob --set-env-vars "MAX_FILE_SIZE=40000M" "MAX_SCAN_SIZE=40000M" "MAX_FILES=50000"
+
+  REM create scanner container app
+  az containerapp create -n clamblob-scanner -g rg-clamblob --yaml "scanner_container_app.yaml" --system-assigned
+
+
+  REM update scanner container app env var
+  az containerapp update -n clamblob-scanner -g rg-clamblob --set-env-var "MOUNT_PATH=/azfile" "APP_INSIGHTS_INSTRUMENTATION_CONN_STRING={appinsights_conn_string}" "CLAMAV_HOST=clamblob-clamav" "CLAMAV_PORT=3310" "QUARANTINE_CONTAINER_NAME=quarantine" "AZURE_STORAGE_NAME={azure_storage_name}" "AZURE_FILE_SHARE_CONN_STRING={azure_file_share_conn_string}" "AZURE_FILE_SHARE_NAME=clamblob-scan" "AZURE_STORAGE_KEY={storage_account_key}"
